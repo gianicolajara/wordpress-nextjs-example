31f7bcb0f165a70553167d2512023bfd64c27fbd
gianicola <gianicolajara@gmail.com>
2022-06-07 21:12:52 -0400

    change title sizes

--- components/PostTitle/index.js
+++ components/PostTitle/index.js
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 
 const PostTitle = ({ children }) => {
-  return <h1 className="text-6xl font-bold py-10">{children}</h1>;
+  return <h1 className="text-4xl lg:text-6xl font-bold py-10">{children}</h1>;
 };
 
 PostTitle.propTypes = {
